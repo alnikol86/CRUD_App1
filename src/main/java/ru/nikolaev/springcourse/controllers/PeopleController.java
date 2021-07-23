1925187b38511c29a8f6edec19d315290661df68
Alexander Nikolaev <nikolaevairman@gmail.com>
2021-07-24 00:02:57 +0300

    CRUD app. PATCH and DELETE requests

--- src/main/java/ru/nikolaev/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/nikolaev/springcourse/controllers/PeopleController.java
@@ -21,14 +21,12 @@ public class PeopleController {
 
     @GetMapping()
     public String index(Model model) {
-        //получаем людей из DAO и передаём на отоброжение в представление
         model.addAttribute("people", personDAO.index());
         return "people/index";
     }
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
-        //Получим одного человека по id из DAO и передадим на отображение в представление
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
     }
@@ -43,4 +41,23 @@ public class PeopleController {
         personDAO.save(person);
         return "redirect:/people";
     }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+        personDAO.update(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        personDAO.delete(id);
+        return "redirect:/people";
+    }
 }
+
